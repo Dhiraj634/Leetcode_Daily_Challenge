ecfefe1b89a9d004bf8221b69c577aca1767c475
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-11-24 09:04:58 +0530

    Modified Code after seeing the solution

--- src/Dhiraj/IntervalListIntersection.java
+++ src/Dhiraj/IntervalListIntersection.java
@@ -1,49 +1,27 @@
 package Dhiraj;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class IntervalListIntersection {
 	public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
 		int x = 0, y=0;
-		ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+		ArrayList<int[]> ans = new ArrayList<>();
 		while(x < firstList.length && y < secondList.length)
 		{
-			if(secondList[y][0] >= firstList[x][0] && secondList[y][0] < firstList[x][1] && secondList[y][1] >= firstList[x][1]){
-				ans.add(new ArrayList<>(Arrays.asList(secondList[y][0],firstList[x][1])));
-				x++;
-			}else if(firstList[x][0] >= secondList[y][0]  && firstList[x][0] < secondList[y][1]   && firstList[x][1] >= secondList[y][1] ){
-				ans.add(new ArrayList<>(Arrays.asList(firstList[x][0],secondList[y][1])));
+			/**
+			 * Below 2 line are important to find the interval if they are intersecting i.e. maxStart and minEnd and then check if they are valid i.e. minEnd >= maxStart
+			 * */
+			int maxStart = Math.max(firstList[x][0],secondList[y][0]);
+			int minEnd= Math.min(firstList[x][1],secondList[y][1]);
+			if(maxStart <= minEnd){
+				ans.add(new int[]{maxStart,minEnd});
+			}
+			if(firstList[x][1] > secondList[y][1]){
 				y++;
-			} else if(secondList[y][0] > firstList[x][0] && secondList[y][1] < firstList[x][1]){
-				ans.add(new ArrayList<>(Arrays.asList(secondList[y][0],secondList[y][1])));
-				y++;
-			}else if(firstList[x][0] > secondList[y][0]  && firstList[x][1] < secondList[y][1]) {
-				ans.add(new ArrayList<>(Arrays.asList(firstList[x][0], firstList[x][1])));
+			}else {
 				x++;
 			}
-//			}else if(firstList[x][0] == secondList[y][0]  && firstList[x][1] == secondList[y][1]){ 	// This case is redundant as it will be covered from first or second
-//				ans.add(new ArrayList<>(Arrays.asList(firstList[x][0],firstList[x][1])));
-//				x++;y++;
-//			}
-			else{
-				if(secondList[y][0] >= firstList[x][1]){
-					if(secondList[y][0] == firstList[x][1])
-						ans.add(new ArrayList<>(Arrays.asList(firstList[x][1],firstList[x][1])));
-					x++;
-				}else if(firstList[x][0] >= secondList[y][1]){
-					if(firstList[x][0] == secondList[y][1])
-						ans.add(new ArrayList<>(Arrays.asList(firstList[x][0],firstList[x][0])));
-					y++;
-				}
-			}
 		}
-		int len = ans.size();
-		int[][] arrayAns = new int[len][2];
-		int m = 0;
-		for(ArrayList<Integer> arr: ans){
-			arrayAns[m++] = arr.stream().mapToInt(Integer::intValue).toArray();
-		}
-		return arrayAns;
+		return ans.toArray(new int[ans.size()][]);
 	}
 }
