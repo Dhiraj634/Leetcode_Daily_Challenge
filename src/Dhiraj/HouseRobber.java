64160f23d0e67782441efc90eb1bf330666b3f37
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-01 10:27:05 +0530

    House Robber optimized

--- src/Dhiraj/HouseRobber.java
+++ src/Dhiraj/HouseRobber.java
@@ -1,22 +1,21 @@
 package Dhiraj;
 
-import java.util.Arrays;
-
 public class HouseRobber {
 	public int rob(int[] nums) {
 		int len = nums.length;
-		int[] dp = new int[len+1];
-		dp[len]=0;
-		dp[len-1] = nums[len-1];
-		int max = -1;
+		// dp[i][0] -> denotes that we are not robbing the current house
+		// dp[i][1] -> denotes that we are robbing the current house
+		int[][] dp = new int[len][2];
+		dp[len-1][0] = 0;
+		dp[len-1][1] = nums[len-1];
 		for(int i=len-2;i>=0;i--){
-			dp[i]= nums[i];
-			int tempMax = 0;
-			for(int j = i+2;j<len;j++){
-				tempMax = Math.max(tempMax, dp[j]);
-			}
-			dp[i]+=tempMax;
+			// If you are not including the current one then you can either rob the next apartment or leave it
+			// and we will decide to rob it or not based on the maximum money that we can get
+			dp[i][0] = Math.max(dp[i+1][1],dp[i+1][0]);
+
+			// if we are robbing the current house then definitely do not rob the next house
+			dp[i][1] = dp[i+1][0] + nums[i];
 		}
-		return Arrays.stream(dp).max().getAsInt();
+		return Math.max(dp[0][0],dp[0][1]);
 	}
 }
