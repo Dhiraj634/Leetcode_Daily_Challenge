8a0e453c896cbd53a41309761425a864783d1301
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-15 11:26:25 +0530

    InsertionSortList Optimized

--- src/Dhiraj/InsertionSortList.java
+++ src/Dhiraj/InsertionSortList.java
@@ -2,46 +2,27 @@ package Dhiraj;
 
 public class InsertionSortList {
 	public ListNode insertionSortList(ListNode head) {
-		ListNode node= head.next, prevNode = head;
-		int sortedLen = 1;
-		if(head.next == null) return head;
-		while(node != null){
-			// Storing the next Node That will be processed
-			ListNode nextNode = node.next;
-			// Removal of the node that we want to put into sorted list
-			prevNode.next = node.next;
-			//Inserting into the sorted list
-			ListNode[] ans = insertIntoLinkedList(head, node, sortedLen,prevNode);
-			sortedLen++;
-			head = ans[0];
-			prevNode = ans[1];
-			node = nextNode;
-		}
-		return head;
-	}
-	private ListNode[] insertIntoLinkedList(ListNode head, ListNode node, int sortedLen, ListNode lastNode){
-		ListNode temp = head, prevNode = null;
-		while(sortedLen > 0){
-			if(temp.val > node.val){
-				break;
+		ListNode dummy = new ListNode();
+		ListNode curr = head;
+
+		while (curr != null) {
+			// At each iteration, we insert an element into the resulting list.
+			ListNode prev = dummy;
+
+			// find the position to insert the current node
+			while (prev.next != null && prev.next.val < curr.val) {
+				prev = prev.next;
 			}
-			sortedLen--;
-			prevNode = temp;
-			temp = temp.next;
+
+			ListNode next = curr.next;
+			// insert the current node to the new list
+			curr.next = prev.next;
+			prev.next = curr;
+
+			// moving on to the next iteration
+			curr = next;
 		}
-		// Case of Inserting in the beginning
-		if(temp == head){
-			node.next = head;
-			head = node;
-		}else{
-			node.next = prevNode.next;
-			prevNode.next = node;
-		}
-		// case of checking the lastNode
-		if(sortedLen == 0){ // Means that the node is inserted at the last and lastNode is updated
-			return new ListNode[]{head, node};
-		}else{ // Means that the node is inserted somewhere in middle and the last and lastNode is not updated
-			return new ListNode[]{head,lastNode};
-		}
+
+		return dummy.next;
 	}
 }
