e2939f5352c83d98f1364a78ab03acdbc17803f1
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-26 10:23:36 +0530

    K Closest Points To Origin

--- src/Dhiraj/KClosestPointsToOrigin.java
+++ src/Dhiraj/KClosestPointsToOrigin.java
@@ -1,2 +1,45 @@
-package Dhiraj;public class KClosestPointsToOrigin {
+package Dhiraj;
+
+import java.util.PriorityQueue;
+
+public class KClosestPointsToOrigin {
+	public int[][] kClosest(int[][] points, int k) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((b,a) -> a[0]*a[0]+a[1]*a[1] - b[0]*b[0] - b[1]*b[1]);
+		for(int i=0;i<k;i++){
+			pq.add(points[i]);
+		}
+		for(int i=k;i<points.length;i++){
+			int[] point = pq.peek();
+			if(point[0]*point[0]+point[1]*point[1] > points[i][0]*points[i][0]+points[i][1]*points[i][1]){
+				pq.poll();
+				pq.add(points[i]);
+			}
+		}
+		int[][] ans = new int[k][2];
+		while(k-->0){
+			ans[k] = pq.poll();
+		}
+		return ans;
+	}
+	/**
+	 * Another Approach
+	 *     public int[][] kClosest(int[][] points, int k) {
+	 *         PriorityQueue<int[]> pq = new PriorityQueue<>((b,a) -> a[0]*a[0]+a[1]*a[1] - b[0]*b[0] - b[1]*b[1]);
+	 *         for(int i=0;i<k;i++){
+	 *             pq.add(points[i]);
+	 *         }
+	 *         for(int i=k;i<points.length;i++){
+	 *             int[] point = pq.peek();
+	 *             if(point[0]*point[0]+point[1]*point[1] > points[i][0]*points[i][0]+points[i][1]*points[i][1]){
+	 *                 pq.poll();
+	 *                 pq.add(points[i]);
+	 *             }
+	 *         }
+	 *         int[][] ans = new int[k][2];
+	 *         while(k-->0){
+	 *             ans[k] = pq.poll();
+	 *         }
+	 *         return ans;
+	 *     }
+	 * */
 }
