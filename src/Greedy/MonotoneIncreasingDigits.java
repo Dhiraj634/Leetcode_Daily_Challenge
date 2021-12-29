3078ba95f01be74cdd4a0deb8a282f677428b1c0
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-29 11:52:19 +0530

    Monotone Increasing Digits

--- src/Greedy/MonotoneIncreasingDigits.java
+++ src/Greedy/MonotoneIncreasingDigits.java
@@ -2,26 +2,32 @@ package Greedy;
 
 public class MonotoneIncreasingDigits {
 	public int monotoneIncreasingDigits(int n) {
+		if(n == 0) return 0;
 		String numStr = String.valueOf(n);
 		StringBuilder newNum = new StringBuilder();
 		int lastIndexModified = -1;
-		char lastChar= numStr.charAt(numStr.length()-1);
+		char[] lastChar= numStr.toCharArray();
+		/** Checking the index which was updated last when traversing from end
+		 *  We will get the index of it and then just reduce that index value by 1
+		 * */
 		for(int i = numStr.length()-2;i>=0;i--){
-			if(numStr.charAt(i) > lastChar){
+			if(lastChar[i] > lastChar[i+1]){
 				lastIndexModified = i;
-				lastChar = (char)(numStr.charAt(lastIndexModified) - 1 );
-			}else{
-				lastChar = numStr.charAt(i);
+				lastChar[i]--;
 			}
 		}
+		// if lastIndexModified == 1, means all the digits are already monotonically increasing
 		if(lastIndexModified == -1) return n;
-		for(int i=0;i<lastIndexModified;i++){
-			newNum.append(numStr.charAt(i));
+
+		// if lastIndexModified != -1 , we add till the lastIndexModified and then add all '9' after that
+		for(int i=0;i<=lastIndexModified;i++){
+			newNum.append(lastChar[i]);
 		}
-		newNum.append((char)(numStr.charAt(lastIndexModified) - 1 ));
+
+		// Adding all '9' after lastIndexModified
 		for(int i=lastIndexModified+1;i<numStr.length();i++){
 			newNum.append("9");
 		}
-		return newNum.length() == 0 ? 0 : Integer.parseInt(newNum.toString());
+		return Integer.parseInt(newNum.toString());
 	}
 }
