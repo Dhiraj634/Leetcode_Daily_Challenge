e07fbeea9e26daefc4a2ab75c6346e9c1c5e8a9d
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2021-12-29 09:13:25 +0530

    Added BFS approaches to PopulatingNextRightPointersInEachNode

--- src/Dhiraj/PopulatingNextRightPointersInEachNode.java
+++ src/Dhiraj/PopulatingNextRightPointersInEachNode.java
@@ -14,4 +14,30 @@ public class PopulatingNextRightPointersInEachNode {
 		pointToNextRightNode(currNode.right, nodeToPointTo == null ? null : nodeToPointTo.left);
 		pointToNextRightNode(currNode.left, currNode.right);
 	}
+
+	/**
+	 * Another Approach using BFS
+	 *
+	 * public Node connect(Node root) {
+	 *         if(root == null) return root;
+	 *         Queue<Node> queue = new LinkedList<>();
+	 *         queue.add(root);
+	 *         while(queue.size() > 0){
+	 *             int size = queue.size();
+	 *             Node leftNode = null;
+	 *
+	 *             // Processing all Node at same level
+	 *             while(size-- > 0){
+	 *                 Node currNode = queue.poll();
+	 *                 if(leftNode != null){
+	 *                     leftNode.next = currNode;
+	 *                 }
+	 *                 if(currNode.left != null)queue.add(currNode.left);
+	 *                 if(currNode.right != null) queue.add(currNode.right);
+	 *                 leftNode = currNode;
+	 *             }
+	 *         }
+	 *         return root;
+	 *     }
+	 * */
 }
