7e4a8576a035db974330a1d84f8d98c06ba30219
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-05 16:13:05 +0530

    Largest Rectangle In Histogram, One pass solution added with comments

--- src/Greedy/LargestRectangleInHistogram.java
+++ src/Greedy/LargestRectangleInHistogram.java
@@ -3,6 +3,12 @@ package Greedy;
 import java.util.Stack;
 
 public class LargestRectangleInHistogram {
+    /**
+     * This can be made single pass , because when we are going from left to right then we already know the
+     * left minimum. But now supposes there comes a value that want to replace the top of the stack so in that case
+     * for the top of the stack the element that is replacing it, is the first smaller element on the right , hence we get our right smaller element.
+     * Had there been any other smaller element before that then the stack's top element would not have been there.
+     * */
     public int largestRectangleArea(int[] heights) {
         int[] leftHeight = new int[heights.length];
         int[] rightHeight = new int[heights.length];
@@ -28,6 +34,27 @@ public class LargestRectangleInHistogram {
         for(int i=0;i<heights.length;i++){
             int tempArea = (rightHeight[i] - leftHeight[i] - 1 ) * heights[i];
             ans = Math.max(ans, tempArea);
+        }
+        return ans;
+    }
+
+    private int largestRectangleAreaSinglePass(int[] heights){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int ans = -1;
+        for(int i=0;i<heights.length;i++){
+            while(stack.peek() !=-1 && heights[stack.peek()] >= heights[i]){
+                int height = heights[stack.pop()];
+                int right = i, left = stack.peek();
+                ans = Math.max(ans, (right - left -1) * height);
+            }
+            stack.push(i);
+        }
+        int index = heights.length;
+        while(stack.peek()!=-1){
+            int height = heights[stack.pop()];
+            int  left = stack.peek();
+            ans = Math.max(ans, (index - left -1) * height);
         }
         return ans;
     }
