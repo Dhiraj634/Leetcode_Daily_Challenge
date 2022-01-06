855e851b2b5504b28d257bc5cb696595b2cceead
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-06 09:06:23 +0530

    Car Pooling Optimized

--- src/January2022/CarPooling.java
+++ src/January2022/CarPooling.java
@@ -1,27 +1,49 @@
 package January2022;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class CarPooling {
     public boolean carPooling(int[][] trips, int capacity) {
+        // Optimized Code
+        // TC: O(nlogn)
         Arrays.sort(trips, (a,b) -> a[1]==b[1] ? a[2]-b[2] : a[1]-b[1]);
-        boolean[] passengerLeft = new boolean[trips.length];
-        int index = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         for(int[] trip: trips){
-            for(int i=0;i<index;i++){
-                if(trips[i][2] <= trip[1] && !passengerLeft[i]){
-                    capacity+=trips[i][0];
-                    passengerLeft[i] = true;
-                }
+            while(!pq.isEmpty() && pq.peek()[2] <= trip[1]){
+                capacity+= pq.poll()[0];
             }
             if(capacity < trip[0]){
                 return  false;
             }
             capacity-=trip[0];
-            index++;
+            pq.add(trip);
 
         }
         return true;
+
+        // Brute force approach
+        // TC : O(n^2)
+        /*
+            Arrays.sort(trips, (a,b) -> a[1]==b[1] ? a[2]-b[2] : a[1]-b[1]);
+            boolean[] passengerLeft = new boolean[trips.length];
+            int index = 0;
+            for(int[] trip: trips){
+                for(int i=0;i<index;i++){
+                    if(trips[i][2] <= trip[1] && !passengerLeft[i]){
+                        capacity+=trips[i][0];
+                        passengerLeft[i] = true;
+                    }
+                }
+                if(capacity < trip[0]){
+                    return  false;
+                }
+                capacity-=trip[0];
+                index++;
+
+            }
+            return true;
+        */
 
     }
 }
