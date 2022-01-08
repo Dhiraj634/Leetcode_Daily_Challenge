24ec1031279c2605a6b5a4e67b184391e2a058d3
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-08 13:03:43 +0530

    Cherry Pick II added DP version

--- src/January2022/CherryPickupII.java
+++ src/January2022/CherryPickupII.java
@@ -11,9 +11,10 @@ public class CherryPickupII {
                Arrays.fill(temp2,-1);
            }
        }
-       return cherryPick(grid,0,0,col-1,dp);
+       return cherryPickRecursion(grid,0,0,col-1,dp);
+//       return cherryPickDp(grid);
     }
-    private int cherryPick(int[][] grid, int row, int col1, int col2, int[][][] dp){
+    private int cherryPickRecursion(int[][] grid, int row, int col1, int col2, int[][][] dp){
         if(row == grid.length) return 0;
         if(dp[row][col1][col2] != -1) return dp[row][col1][col2];
 
@@ -22,11 +23,30 @@ public class CherryPickupII {
         int max = 0;
         for(int newCol1 = Math.max(col1-1,0);newCol1 < grid[0].length && newCol1 <= col1+1;newCol1++){
             for(int newCol2 = Math.max(col2-1,0);newCol2 < grid[0].length && newCol2 <= col2+1;newCol2++){
-                max = Math.max(max, cherryPick(grid,row+1, newCol1, newCol2,dp));
+                max = Math.max(max, cherryPickRecursion(grid,row+1, newCol1, newCol2,dp));
             }
         }
         result+=max;
         dp[row][col1][col2] = result;
         return result;
+    }
+    private int cherryPickDp(int[][] grid){
+        int rows = grid.length, cols = grid[0].length;
+        int[][][] dp = new int[rows +1][cols][cols];
+        for(int row = rows - 1; row >=0; row--){
+            for(int col1 = 0; col1 < cols; col1++){
+                for(int col2 = 0; col2 < cols; col2++){
+                    dp[row][col1][col2] = col1 == col2 ? grid[row][col2] + grid[row][col1] : grid[row][col2];
+                    int max = 0;
+                    for(int newCol1 = Math.max(col1-1,0);newCol1 <= Math.min(col1+1,grid[0].length-1);newCol1++){
+                        for(int newCol2 = Math.max(col2-1,0);newCol2 <= Math.min(col2+1,grid[0].length-1);newCol2++){
+                            max = Math.max(max, dp[row +1][newCol1][newCol2]);
+                        }
+                    }
+                    dp[row][col1][col2]+=max;
+                }
+            }
+        }
+        return dp[0][0][cols -1];
     }
 }
