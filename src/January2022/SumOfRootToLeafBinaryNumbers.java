9c2a42e4aaf686e7f6648243edb07d4c9a035217
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-11 10:07:06 +0530

    Added Morris preorder traversal algorithm

--- src/January2022/SumOfRootToLeafBinaryNumbers.java
+++ src/January2022/SumOfRootToLeafBinaryNumbers.java
@@ -8,6 +8,40 @@ public class SumOfRootToLeafBinaryNumbers {
         dfs(root,0);
         return ans;
     }
+    private int morrisPreorderTraversal(TreeNode root){
+        int val = 0;
+        int ans = 0;
+        TreeNode backDoor;
+        while(root != null){
+            if(root.left !=null){
+                backDoor = root.left;
+                int steps = 1;
+                while(backDoor.right != null && backDoor.right != root){
+                    backDoor=backDoor.right;
+                    steps++;
+                }
+                if(backDoor.right == root){
+                    if(backDoor.left == null){
+                        ans+=val;
+                    }
+                    val >>= steps; // Backtracking
+                    backDoor.right = null;
+                    root = root.right;
+                }else{
+                    backDoor.right = root;
+                    val = (val << 1) + root.val;
+                    root = root.left;
+                }
+            }else{
+                val = (val << 1) + root.val;
+                if (root.right == null) {
+                    ans += val;
+                }
+                root = root.right;
+            }
+        }
+        return ans;
+    }
     private void  dfs(TreeNode root, int num){
         if(root == null) return;
         if(root.left == null && root.right == null){
@@ -16,4 +50,5 @@ public class SumOfRootToLeafBinaryNumbers {
        dfs(root.left,(num<<1)+root.val);
        dfs(root.right,(num<<1)+root.val);
     }
+
 }
