809cea61934d7fbc4d2957853add4518e1ad9fcf
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-14 08:49:36 +0530

    String To Integer

--- src/January2022/StringToInteger.java
+++ src/January2022/StringToInteger.java
@@ -2,22 +2,26 @@ package January2022;
 
 public class StringToInteger {
     public int myAtoi(String s) {
+        // If length == 0 return 0
         if(s.length() == 0) return 0;
-        boolean isPositive = true;
         int index = 0;
+
+        // Checking for whitespaces
         while(index < s.length() && s.charAt(index)==' ')index++;
         if(s.length() == index) return 0;
+
+        // Checking for positive or negative number
+        boolean isPositive = true;
         if(s.charAt(index) == '-')
         {
             isPositive = false;index++;
         }
         else if(s.charAt(index) == '+')
         {
-            isPositive = true;index++;
+           index++;
         }
-        else{
-            isPositive = true;
-        }
+
+        //Getting the content of the string
         long value = 0;
         while(index < s.length() && Character.isDigit(s.charAt(index))){
 
