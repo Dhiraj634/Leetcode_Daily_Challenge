5fb428b7331abea7397d4fb0cb9d0d8a730ff9c0
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-17 09:17:13 +0530

    Word pattern Optimized

--- src/January2022/WordPattern.java
+++ src/January2022/WordPattern.java
@@ -5,22 +5,20 @@ import java.util.HashSet;
 
 public class WordPattern {
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character,HashSet<String>> map = new HashMap<>();
-        HashMap<String,HashSet<Character>> strMap = new HashMap<>();
+        HashMap<Character,String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         String[] str = s.split(" ");
         if(str.length!= pattern.length()) return false;
         for(int i=0;i<str.length;i++){
-            char ch= pattern.charAt(i);
-            map.putIfAbsent(ch,new HashSet<>());
-            strMap.putIfAbsent(str[i],new HashSet<>());
-            map.get(ch).add(str[i]);
-            strMap.get(str[i]).add(ch);
-        }
-        for(char ch: map.keySet()){
-            if(map.get(ch).size() > 1) return false;
-        }
-        for(String str1: strMap.keySet()){
-            if(strMap.get(str1).size() > 1) return false;
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(str[i]))return false;
+            }else{
+                if(set.contains(str[i])) return false;
+            }
+            map.put(ch,str[i]);
+            set.add(str[i]);
+
         }
         return true;
     }
