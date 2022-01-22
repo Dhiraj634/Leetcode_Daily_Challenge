09526828e3e2f1b15f8e74869fc25a9a33528b1b
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-22 09:32:59 +0530

    Stone Game IV DP and optimized

--- src/January2022/StoneGameIV.java
+++ src/January2022/StoneGameIV.java
@@ -3,13 +3,15 @@ package January2022;
 public class StoneGameIV {
     int[][] dp;
     public boolean winnerSquareGame(int n) {
+        return 0 == getWinnerDP(n);
+    }
+    private int getWinnerRecursionWithMemo(int n){
         dp = new int[n+1][2];
         for(int[] row : dp){
             row[0]=-1;
             row[1]=-1;
         }
-        int winner = getWinner(n,0);
-        return winner ==0;
+        return getWinner(n,0);
     }
     private int getWinner(int n,int turn){
         if(n == 0){
@@ -29,5 +31,33 @@ public class StoneGameIV {
         }
         dp[n][turn] = tempWinner;
         return tempWinner;
+    }
+    private int getWinnerDP(int n){
+        int[][] dp = new int[n+1][2];
+        dp[0][1] = 0;
+        dp[0][0] = 1;
+        for(int i = 1;i<n+1;i++){
+            int turn = 0;
+            int tempWinner = turn^1;
+            for(int j = 1;j*j<=i;j++){
+                tempWinner = tempWinner & dp[i - j*j][ turn^1];
+                if(tempWinner == turn){
+                    dp[i][turn] = turn;
+                    break;
+                }
+            }
+            if(tempWinner != turn) dp[i][turn] =tempWinner;
+            turn = 1;
+            tempWinner = 0;
+            for(int j = 1;j*j<=i;j++){
+                tempWinner = tempWinner | dp[i - j*j][ turn^1];
+                if(tempWinner == turn){
+                    dp[i][turn] = turn;
+                    break;
+                }
+            }
+            if(tempWinner != turn) dp[i][turn] =tempWinner;
+        }
+        return dp[n][0];
     }
 }
