ac776741f3abbe1d9efe88355e5f72db1d2d877f
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-01-31 08:53:20 +0530

    Richest Customer Wealth, one liner code added

--- src/January2022/RichestCustomerWealth.java
+++ src/January2022/RichestCustomerWealth.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class RichestCustomerWealth {
     public int maximumWealth(int[][] accounts) {
+//        return Arrays.stream(accounts).map(input -> Arrays.stream(input).sum()).mapToInt(Integer::intValue).max().getAsInt();
+//        return Arrays.stream(accounts).map(input -> Arrays.stream(input).sum()).reduce(-1, Math::max);
         int max = -1;
         for(int[] wealth : accounts){
             int sum = Arrays.stream(wealth).sum();
